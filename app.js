66008d4a9d48986d498e749b2d84de23d19aec3f
tyk2013 <ken.rad0202@gmail.com>
2016-08-06 13:17:05 +0900

    微調整

--- app.js
+++ app.js
@@ -141,51 +141,51 @@
         manage.getAddress(text, function(result){
             //メッセージの部分
             var messageData = {
-                attachment: {
-                    type: "template",
-                    payload: {
-                        template_type: "generic",
-                        elements: [{
-                            title: "rift",
-                            subtitle: "Next-generation virtual reality",
-                            item_url: "https://www.oculus.com/en-us/rift/",
-                            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-                            buttons: [{
-                                type: "web_url",
-                                url: "https://www.oculus.com/en-us/rift/",
-                                title: "Open Web URL"
-                            }, {
-                                type: "postback",
-                                title: "Call Postback",
-                                payload: "Payload for first bubble",
-                            }],
-                        }, {
-                            title: "touch",
-                            subtitle: "Your Hands, Now in VR",
-                            item_url: "https://www.oculus.com/en-us/touch/",
-                            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-                            buttons: [{
-                                type: "web_url",
-                                url: "https://www.oculus.com/en-us/touch/",
-                                title: "Open Web URL"
-                            }, {
-                                type: "postback",
-                                title: "Call Postback",
-                                payload: "Payload for second bubble",
-                            }]
-                        }]
-                    }
-                }
-                // attachment:{
-                //     type:"audio",
-                //     payload:{
-                //         url: result.results[0].previewUrl
+                // attachment: {
+                //     type: "template",
+                //     payload: {
+                //         template_type: "generic",
+                //         elements: [{
+                //             title: "rift",
+                //             subtitle: "Next-generation virtual reality",
+                //             item_url: "https://www.oculus.com/en-us/rift/",
+                //             image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+                //             buttons: [{
+                //                 type: "web_url",
+                //                 url: "https://www.oculus.com/en-us/rift/",
+                //                 title: "Open Web URL"
+                //             }, {
+                //                 type: "postback",
+                //                 title: "Call Postback",
+                //                 payload: "Payload for first bubble",
+                //             }],
+                //         }, {
+                //             title: "touch",
+                //             subtitle: "Your Hands, Now in VR",
+                //             item_url: "https://www.oculus.com/en-us/touch/",
+                //             image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+                //             buttons: [{
+                //                 type: "web_url",
+                //                 url: "https://www.oculus.com/en-us/touch/",
+                //                 title: "Open Web URL"
+                //             }, {
+                //                 type: "postback",
+                //                 title: "Call Postback",
+                //                 payload: "Payload for second bubble",
+                //             }]
+                //         }]
                 //     }
                 // }
+                attachment:{
+                    type:"audio",
+                    payload:{
+                        url: result.results[0].previewUrl
+                    }
+                }
             };
             //フェイスブックページのメッセの返答部分
             request({
-                url: 'https://graph.facebook.com/v2.6/me/messages',
+                url: 'https://graph.facebook.com/v2.7/me/messages',
                 qs: {access_token:token},
                 method: 'POST',
                 json: {
